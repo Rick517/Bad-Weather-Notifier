b02fdc184ea897146da84e23c95971ec14e39697
Dmitry Savkov <dmitry_savkov@mail.ru>
2024-08-25 09:55:14 +0500

    Modified routes.py to server server website and removed /server configuration from vite. Serving https server...

--- react-client/vite.config.js
+++ react-client/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/server': {
-        target: 'https://bad-weather-notifier-server.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/server/, ''),
-      },
-    },
-  },
 })
