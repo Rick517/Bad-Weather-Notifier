6680a1f667f50789d03d03ab4b1443751dab98ab
Dmitry Savkov <dmitry_savkov@mail.ru>
2024-08-25 09:15:25 +0500

    Gunicorn added.

--- flask-server/run.py
+++ flask-server/run.py
@@ -1,8 +1,5 @@
 from server import app, db
 
-if __name__ == '__main__':
-    # I need server prefix because react client has own routes.
-    with app.app_context():
-        db.create_all()
-        db.session.commit()
-    app.run(port=5000, host="0.0.0.0")
+with app.app_context():
+    db.create_all()
+    db.session.commit()
