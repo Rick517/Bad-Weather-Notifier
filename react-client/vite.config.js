2f76057aa7323271f816bf5ffdfb2d26902e53a9
Dmitry Savkov <dmitry_savkov@mail.ru>
2024-08-25 09:36:17 +0500

    Corrected url to server

--- react-client/vite.config.js
+++ react-client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/server': {
-        target: 'https://bad-weather-notifier.onrender.com',
+        target: 'https://bad-weather-notifier-server.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/server/, ''),
       },
